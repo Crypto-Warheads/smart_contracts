165656ef00bf32e69941fd29ff84b597e3e99200
Yash Goyal <yashgo0018@gmail.com>
2024-06-02 10:23:28 +0200

    added some more network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,11 +13,39 @@ const config: HardhatUserConfig = {
       url: "https://eth-sepolia.g.alchemy.com/v2/PwLFgwHhfPYXl7Zbr5-6lTRciwNkGVpl",
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
+    mantle: {
+      url: "https://rpc.mantle.xyz",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    linea: {
+      url: `https://linea-mainnet.infura.io/v3/4207fe007a0c4f428558e8680104a75e`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY || ""
-    }
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      mantle: "mantle",
+      linea: process.env.LINEASCAN_API_KEY || ""
+    },
+    customChains: [
+      {
+        network: "mantle",
+        chainId: 5000,
+        urls: {
+          apiURL: "https://api.routescan.io/v2/network/mainnet/evm/5000/etherscan",
+          browserURL: "https://mantlescan.info"
+        }
+      },
+      {
+        network: "linea",
+        chainId: 59144,
+        urls: {
+          apiURL: "https://api.lineascan.build/api",
+          browserURL: "https://lineascan.build/"
+        }
+      }
+    ]
   },
 };
 
